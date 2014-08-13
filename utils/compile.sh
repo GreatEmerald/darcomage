ef93a81467ac14de07a60964bbc922d2065e501b
GreatEmerald <pastas4@gmail.com>
2014-08-13 10:47:33 +0300

    Last fixes to make it compile and run
    Yay, finally! Only the menu so far, but the rest of the logic isn't far off
    either. Also, since it goes ingame now, this is the first Alpha commit.
    
    Version: 1.alpha.02.00
    
    Signed-off-by: GreatEmerald <pastas4@gmail.com>

--- utils/compile.sh
+++ utils/compile.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 cd ../src
-dmd -m64 -od../bin/linux-x86_64 -ofdarcomage \
- -L-lGL -L-lSDL2 -L-lpthread -L-lSDL2_ttf -L-lSDL2_image \
+dmd -m64 -ofdarcomage \
  -I../include -I../../libarcomage/src -I../../libarcomage/include/LuaD \
- *.d
+ -L-lGL -L-lSDL2 -L-lpthread -L-lSDL2_ttf -L-lSDL2_image -L-llua -L-ldl \
+ *.d ../../libarcomage/src/*.d \
+ ../../libarcomage/include/LuaD/luad/*.d ../../libarcomage/include/LuaD/luad/conversions/*.d ../../libarcomage/include/LuaD/luad/c/*.d \
+ ../include/derelict/sdl2/*.d ../include/derelict/opengl3/*.d ../include/derelict/util/*.d
+rm darcomage.o
+mv darcomage ../bin/linux-x86_64
 cd ../utils
